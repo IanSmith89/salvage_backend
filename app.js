53874aee68d2b70f596eaefe38c357370067e40d
Ian Smith <iansmith1026@gmail.com>
2016-03-30 18:16:42 -0600

    final changes

--- app.js
+++ app.js
@@ -122,8 +122,8 @@ app.use(bodyParser.json());
 app.use(methodOverride());
 
 var corsOptions = {
-  // origin: 'http://localhost:8080'
-  origin: process.env.FRONTEND
+  origin: 'http://localhost:8080'
+  // origin: process.env.FRONTEND
 };
 app.use(cors(corsOptions));
 
